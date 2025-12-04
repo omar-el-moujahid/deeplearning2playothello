f350042d87cb21a2f2add7fe4dcf2c0b2a31482d
Bilal MARGHCIH <e2508045@ensimlan.univ-lemans.fr>
2025-12-04 23:14:25 +0100

    500 batch result

--- automatique_training.py
+++ automatique_training.py
@@ -6,7 +6,7 @@ learning_rates = [0.1, 0.01, 0.005, 0.001, 0.0001]
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
-batch_size = 1000
+batch_size = 500
 
 for lr in learning_rates:
     lr_str = str(lr).replace('.', '_')
