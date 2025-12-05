52ea691c25b4893831a6604bdcf2a7f34e7dd588
omar-el-moujahid <omarelmoujahid5@gmail.com>
2025-12-05 14:35:16 +0100

    MLP training

--- training_One2One.py
+++ training_One2One.py
@@ -27,7 +27,9 @@ dataset_conf["filelist"]="train.txt"
 dataset_conf["len_samples"]=len_samples
 dataset_conf["path_dataset"]="dataset\dataset/"
 # dataset_conf['batch_size']=1000
-dataset_conf['batch_size']=args.batch_size
+# dataset_conf['batch_size']=args.batch_size
+dataset_conf['batch_size']=500
+
 
 print("Training Dataste ... ")
 ds_train = CustomDatasetOne(dataset_conf,load_data_once4all=True)
@@ -40,7 +42,8 @@ dataset_conf["filelist"]="dev.txt"
 dataset_conf["len_samples"]=len_samples
 dataset_conf["path_dataset"]="dataset\dataset/"
 # dataset_conf['batch_size']=1000
-dataset_conf['batch_size']=args.batch_size
+# dataset_conf['batch_size']=args.batch_size
+dataset_conf['batch_size']=500
 
 print("Development Dataste ... ")
 ds_dev = CustomDatasetOne(dataset_conf,load_data_once4all=True)
@@ -56,8 +59,18 @@ conf["LSTM_conf"]={}
 conf["LSTM_conf"]["hidden_dim"]=128
 
 model = MLP(conf).to(device)
-##opt = torch.optim.Adam(model.parameters(), lr=0.001)
-opt = torch.optim.Adam(model.parameters(), lr=args.lr)
+##opt = torch.optim.Adam(model.parameters(), lr=0.00)
+# opt = torch.optim.Adam(model.parameters(), lr=args.lr)
+# opt = torch.optim.AdamW(
+#     model.parameters(),
+#     lr=0.001,
+#     weight_decay=1e-5  # Regularization via weight decay
+# )
+
+# Define learning rate scheduler
+# scheduler = torch.optim.lr_scheduler.StepLR(opt, step_size=10, gamma=0.5)
+# opt = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, nesterov=True, weight_decay=1e-5)
+opt = torch.optim.RMSprop(model.parameters(), lr=0.001, weight_decay=1e-5)
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -68,7 +81,7 @@ print("Number of parameters: %s" % n)
 best_epoch=model.train_all(trainSet,
                        devSet,
                        conf['epoch'],
-                       device, opt)
+                       device, opt )
 
 # model = torch.load(conf["path_save"] + '/model_2.pt')
 # model.eval()
